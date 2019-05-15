2799c612317f326b7205c71a4224e4e78fb433f8
keyakko <keyakko.web@gmail.com>
2019-05-16 00:27:18 +0900

    Add hello section

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,14 @@
 <template>
   <nuxt />
 </template>
+
+<style lang="scss">
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply font-light;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <section class="container">
-    cosylab ob team
-  </section>
+  <main>
+    <section class="container">
+      <div class="title">
+        <p>Hello!</p>
+        <p>We are Cosylab-OB team😏</p>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+main {
+  @apply w-screen h-screen bg-teal-darkest;
+  section.container {
+    @apply w-screen h-full text-white flex mx-auto items-center;
+    min-height: 400px;
+    .title {
+      @apply text-3xl leading-normal;
+    }
+  }
+}
+</style>
